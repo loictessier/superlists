bb26281e1679cdb4466f49492e4e52b9446e15ab
Loïc Tessier <loictessier.inge@gmail.com>
2020-09-21 18:33:39 +0200

    use bootstrap to improve layout

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -1,5 +1,5 @@
 
-from django.test import LiveServerTestCase
+from django.contrib.staticfiles.testing import StaticLiveServerTestCase
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.common.exceptions import WebDriverException
@@ -8,7 +8,7 @@ import time
 MAX_WAIT = 10
 
 
-class NewVisitortest(LiveServerTestCase):
+class NewVisitortest(StaticLiveServerTestCase):
 
     def setUp(self):
         self.browser = webdriver.Firefox()
