ffe3d0d4ea08f8814e9cd161bb2a1523242a3871
Loïc Tessier <loictessier.inge@gmail.com>
2020-09-30 09:22:17 +0200

    spiked in custom passwordless auth backend

--- superlists/settings.py
+++ superlists/settings.py
@@ -37,6 +37,12 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'lists',
+    'accounts',
+]
+
+AUTH_USER_MODEL = 'accounts.ListUser'
+AUTHENTICATION_BACKENDS = [
+    'accounts.authentication.PasswordlessAuthenticationBackend'
 ]
 
 MIDDLEWARE = [
@@ -119,3 +125,30 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.abspath(os.path.join(BASE_DIR, '../static'))
+
+# Email
+
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST_USER = 'intairemezzo.website@gmail.com'
+EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_PASSWORD')
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+
+# Logging
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'console': {
+            'level': 'DEBUG',
+            'class': 'logging.StreamHandler',
+        },
+    },
+    'loggers': {
+        'django': {
+            'handlers': ['console'],
+        },
+    },
+    'root': {'level': 'INFO'},
+}
--- superlists/urls.py
+++ superlists/urls.py
@@ -17,8 +17,10 @@ from django.urls import path, include
 
 from lists import views as list_views
 from lists import urls as list_urls
+from accounts import urls as accounts_urls
 
 urlpatterns = [
     path('', list_views.home_page, name='home'),
     path('lists/', include(list_urls)),
+    path('accounts/', include(accounts_urls)),
 ]
