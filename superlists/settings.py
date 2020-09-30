17c8b3eda391c5a83eba928cb1dae42c6378bb74
Loïc Tessier <loictessier.inge@gmail.com>
2020-09-30 11:11:46 +0200

    new app accounts, new navbar in base.html, logging config in settings.py

--- superlists/settings.py
+++ superlists/settings.py
@@ -119,3 +119,23 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.abspath(os.path.join(BASE_DIR, '../static'))
+
+
+# Logging
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'console': {
+            'level': 'DEBUG',
+            'class': 'logging.StreamHandler',
+        },
+    },
+    'loggers': {
+        'django': {
+            'handlers': ['console'],
+        },
+    },
+    'root': {'level': 'INFO'},
+}
